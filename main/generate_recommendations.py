5567bfe84668adf8459e9455ecc13a1f570734d6
kishalaybanerjee <kishalaybanerjee22@gmail.com>
2021-12-05 19:11:31 +0000

    add recommendation function and list further work

--- main/generate_recommendations.py
+++ main/generate_recommendations.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from text_comparison import parseDataIntoDict, filterDataDict
 
@@ -8,17 +9,36 @@ def getSimilarityDict(inPath):
         return json.loads(fp.read())
 
 
-def getShowNamesInCorrectFormat(dataDict):
-    showList = list(dataDict)
-    for shows in showList:
-        name = " ".join(dataDict[shows]['title'])
-        dataDict[shows]['title'] = name
+def convertNoneTypeToString(value):
+    return value if value else ''
 
 
 def getShowDetails(dataPath, jsonPath):
     dataDict = filterDataDict(parseDataIntoDict(dataPath))
     showSimilarityDict = getSimilarityDict(jsonPath)
+
+    showList = list(dataDict.keys())
+    chosenShow = random.choice(showList)
+    name = dataDict[chosenShow]['title']
+
+    print(f'Searching for shows similar to {name} based on cast...')
     print()
+
+    try:
+        similarShowsDict = dict(sorted(showSimilarityDict[chosenShow].items(), key=lambda item: item[1], reverse=True))
+        keyList = list(similarShowsDict)
+        if len(keyList) >= 3:
+            simShow = [dataDict[keyList[0]]['title'], dataDict[keyList[1]]['title'], dataDict[keyList[2]]['title']]
+        else:
+            simShow = [dataDict[keyList[0]]['title'], None, None]
+    except KeyError:
+        print(f'Could not find similar shows to {name} based on cast. Please try again')
+
+    # handler to prevent printing None when there are less than 3 similar shows
+    simShow = [convertNoneTypeToString(i) for i in simShow]
+
+    print(f'According to cast, shows similar to {name} are: '
+          f'{str(simShow[0])} \n {str(simShow[1])} \n {str(simShow[2])}')
 
 
 if __name__ == '__main__':
@@ -26,3 +46,7 @@ if __name__ == '__main__':
     dataPath = './netflix_titles.csv'
     jsonPath = './show_similarities.json'
     getShowDetails(dataPath, jsonPath)
+
+    # TODO: Add user input for show name instead of random.choice (need to know the names in the database?)
+    # TODO: Check the value of the highest similarity before reporting, if max is less than 0.25 (example), is there any point in reporting?
+    # TODO: Replace JS with better metrics
